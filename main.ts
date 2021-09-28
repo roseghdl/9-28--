809440ab35a2943ee328ef78dfd53a68d4f923d1
roseghdl <90684967+roseghdl@users.noreply.github.com>
2021-09-28 11:13:18 +0900

    LED3개

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
 basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+    music.playTone(523, music.beat(BeatFraction.Half))
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P5, 1)
+    music.playTone(523, music.beat(BeatFraction.Half))
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P5, 0)
     music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(523, music.beat(BeatFraction.Whole))
-    music.playTone(523, music.beat(BeatFraction.Eighth))
+})
+basic.forever(function () {
+	
 })
