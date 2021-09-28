3a391df8b1b3b3430e549b28cf8e8fff9ea932c1
roseghdl <90684967+roseghdl@users.noreply.github.com>
2021-09-28 09:41:06 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function () {
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+})
